4c5b7e34a20ab8571b509b1869e7f393a0fad2f3
velatop <68625195+velatop@users.noreply.github.com>
2025-04-01 04:00:57 -0400

    Add week 2 project

---
+++ Week 2/Week 2 - Programming Lab - Cristhian Carcamo/Week2-Carcamo_Solutionn/Week2-Carcamo_Project/frmSummer2023.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+//Name: Cristhian Carcamo
+// Class: Object Oriented With c#
+// Due: 3/5/2025
+// Semester: Spring 2025
+
+namespace Week2_Carcamo_Project
+{
+    public partial class frmSummer2023 : Form
+    {
+        public frmSummer2023()
+        {
+            InitializeComponent();
+        }
+        private void btnWipeClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = txtInput.Text;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
